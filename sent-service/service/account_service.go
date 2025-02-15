f3272afdf85dfab3721364ee2eefffc905c6e14d
ddevin789 <devin@darasa.io>
2025-02-15 15:31:09 +0700

    Update account_service.go

--- sent-service/service/account_service.go
+++ sent-service/service/account_service.go
@@ -11,7 +11,7 @@ import (
 	"time"
 )
 
-var ttl = time.Duration(24) * time.Hour
+var ttl = 24 * time.Hour
 
 type AccountService interface {
 	CreateAccount(ctx context.Context, req *proto.CreateAccountRequest) (*proto.CreateAccountResponse, error)
@@ -29,17 +29,37 @@ func NewAccountService(client client.AccountClient, fw *filewriter.FileWriter) A
 
 func (s *accountService) CreateAccount(ctx context.Context, req *proto.CreateAccountRequest) (*proto.CreateAccountResponse, error) {
 	slog.Info(util.StringPattern("Account service >> %d", req.AccountId))
-	go s.grpcclient.CreateAccount(context.Background(), req)
-	return &proto.CreateAccountResponse{Code: 0, Message: "Success"}, nil
+
+	// Sử dụng context với timeout hợp lý
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	resp, err := s.grpcclient.CreateAccount(ctx, req)
+	if err != nil {
+		slog.Error(fmt.Sprintf("gRPC CreateAccount failed: %s", err.Error()))
+		return &proto.CreateAccountResponse{Code: -1, Message: "Failed"}, err
+	}
+
+	return resp, nil
 }
 
 func (s *accountService) BalanceChange(ctx context.Context, req *proto.BalanceChangeRequest) (*proto.BalanceChangeResponse, error) {
-	// transactionId, account id, amount, action, status
+	// Ghi log trạng thái xử lý
 	line := util.StringPattern("%d_%d_%d_%d_%s", req.Tx, req.Ac, req.Am, req.Act, "PROCESSING")
 	err := s.fw.WriteLine(line)
 	if err != nil {
 		slog.Error(fmt.Sprintf("write processing file has an error %s", err.Error()))
 	}
-	go s.grpcclient.BalanceChange(util.ContextwithTimeout(), req)
-	return &proto.BalanceChangeResponse{Code: 0, Message: "Success"}, nil
+
+	// Sử dụng context với timeout hợp lý
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	resp, err := s.grpcclient.BalanceChange(ctx, req)
+	if err != nil {
+		slog.Error(fmt.Sprintf("gRPC BalanceChange failed: %s", err.Error()))
+		return &proto.BalanceChangeResponse{Code: -1, Message: "Failed"}, err
+	}
+
+	return resp, nil
 }
